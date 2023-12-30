1cd3c800a4e43066b2daa9e13f64f49ef82cd339
Laura-lufeng <laura_lufeng@outlook.com>
2023-12-31 05:03:57 +0800

    update my blog website for HKJC interview
    rebuilt a website based on vite

---
+++ personal Tailor/aboutMe.md
@@ -0,0 +1,5 @@
+# 👋 Hi, I'm Laura Feng, a motivated data analyst. I am passionate about building models to solve problems. Relevant skills include problem solving, programming and creative thinking.
+
+- 👀 I’m interested in ⌨️coding | 💪gym and musical
+- I'm interested in technologies for process automation such as `Python` , `R` , `VBA`, and expoloring visualization.
+- If you are interested in my blog or project, feel free to contact me through the contact details below, welcome to discuss great and interesting automation technologies. Think Big, Start Small, Learn Fast.
---
+++ src/views/NotFound.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+</script>
+
+<template>
+  <div class="notFound">
+    <div class="container">
+      <h1>404</h1>
+      <p>NOT FOUND</p>
+      <el-divider></el-divider>
+      <br />
+      <p>要一直 努力努力 永不放棄 才可以 改變世界 C'mon 改變自己</p>
+      <el-button size="large" @click="router.push('/')"
+        >Return With Me</el-button
+      >
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.notFound {
+  height: calc(100vh - 64px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .container {
+    width: 60vw;
+    text-align: center;
+
+    h1 {
+      font-size: 300%;
+    }
+    p {
+      font-style: italic;
+      opacity: 0.5;
+    }
+    p:last-of-type {
+      margin-bottom: 20px;
+    }
+  }
+}
+</style>
